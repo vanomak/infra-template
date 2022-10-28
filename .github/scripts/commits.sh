7c9ea0dd3372646b4e40fbaba1fa15b3a999aa9e
Ivan Makarov <vanomak@gmail.com>
2022-10-28 15:47:04 +1000

    bin bash

--- .github/scripts/commits.sh
+++ .github/scripts/commits.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 tag1=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | head -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
 tag2=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | tail -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
 git log --pretty=format:"Ответственный за релиз: %an%n" $tag1...$tag2 | head -1
