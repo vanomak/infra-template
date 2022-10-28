17fe702a14a432ff3c6245cf516eaad0b8172cbe
Ivan Makarov <vanomak@gmail.com>
2022-10-28 13:45:06 +1000

    utf8

--- .github/scripts/reporter.js
+++ .github/scripts/reporter.js
@@ -28,7 +28,7 @@ function updateIssueDescription(trackerToken, trackerIssueId, trackerOrgId, desc
     });
 }
 
-const contents = fs.readFileSync('./content.txt').toString();
+const contents = fs.readFileSync('./content.txt', {encoding: 'utf8'});
 updateIssueDescription(
   process.env.TRACKER_TOKEN,
   process.env.TRACKER_ISSUE_ID,
