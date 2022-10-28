2143cf1de970bd87293df1e88dd5964293fa1a00
Ivan Makarov <vanomak@gmail.com>
2022-10-28 17:53:33 +1000

    коммиты, попавшие в релиз

--- .github/scripts/commits.sh
+++ .github/scripts/commits.sh
@@ -2,4 +2,5 @@
 tag1=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | head -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
 tag2=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | tail -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
 git log --pretty=format:"Ответственный за релиз: %an%n" $tag1...$tag2 | head -1
+echo "коммиты, попавшие в релиз:"
 git log --pretty=format:"%H %an %s%n" $tag2...$tag1
