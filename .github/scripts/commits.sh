8c5f8207a7822e7f357be74abefe595258f97a13
Ivan Makarov <vanomak@gmail.com>
2022-10-28 16:45:58 +1000

    git replace

--- .github/scripts/commits.sh
+++ .github/scripts/commits.sh
@@ -3,4 +3,3 @@ tag1=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | head -1 | grep -o
 tag2=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | tail -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
 git log --pretty=format:"Ответственный за релиз: %an%n" $tag1...$tag2 | head -1
 git log --pretty=format:"%H %an %s%n" $tag2...$tag1
-ls -al
