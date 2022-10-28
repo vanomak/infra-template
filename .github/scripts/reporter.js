075dbc1ce972f0040f2a3c685127597af93c5885
Ivan Makarov <vanomak@gmail.com>
2022-10-28 12:27:07 +1000

    await refactor

--- .github/scripts/reporter.js
+++ .github/scripts/reporter.js
@@ -1,29 +1,28 @@
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-async function updateIssueDescription(trackerToken, trackerIssueId, trackerOrgId, description) {
-  try {
-    await fetch(`https://api.tracker.yandex.net/v2/issues/${trackerIssueId}`, {
-      headers: {
-        "Authorization": `Bearer ${trackerToken}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-        "X-Org-ID": trackerOrgId,
-      },
-      method: "PATCH",
-      body: JSON.stringify({
-        description
-      })
-    });
-    console.log("Задача в трекере успешно обновлена");
-  } catch (error) {
-    console.log("Не удалось обновить задачу", error.getMessage);
-  }
+function updateIssueDescription(trackerToken, trackerIssueId, trackerOrgId, description) {
+  return fetch(`https://api.tracker.yandex.net/v2/issues/${trackerIssueId}`, {
+    headers: {
+      "Authorization": `Bearer ${trackerToken}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-Org-ID": trackerOrgId,
+    },
+    method: "PATCH",
+    body: JSON.stringify({
+      description
+    })
+  });
 }
 
 const contents = fs.readFileSync('./content.txt');
-await updateIssueDescription(
+updateIssueDescription(
   process.env.TRACKER_TOKEN,
   process.env.TRACKER_ISSUE_ID,
   process.env.TRACKER_ORG_ID,
   contents
-);
+).then(() => {
+  console.log("Задача в трекере успешно обновлена");
+}).catch(error => {
+  console.log("Не удалось обновить задачу", error.getMessage);
+});
