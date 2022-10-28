1d31eb5a006d65dfeeaa12c77793bb07758e7791
Ivan Makarov <vanomak@gmail.com>
2022-10-28 12:16:48 +1000

    реализовано добавление задачи

--- .github/scripts/reporter.js
+++ .github/scripts/reporter.js
@@ -1,3 +1,29 @@
 const fs = require("fs");
+const fetch = require("node-fetch");
+
+async function updateIssueDescription(trackerToken, trackerIssueId, trackerOrgId, description) {
+  try {
+    await fetch(`https://api.tracker.yandex.net/v2/issues/${trackerIssueId}`, {
+      headers: {
+        "Authorization": `Bearer ${trackerToken}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Org-ID": trackerOrgId,
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        description
+      })
+    });
+    console.log("Задача в трекере успешно обновлена");
+  } catch (error) {
+    console.log("Не удалось обновить задачу", error.getMessage);
+  }
+}
+
 const contents = fs.readFileSync('./content.txt');
-console.log(contents);
+await updateIssueDescription(
+  process.env.TRACKER_TOKEN,
+  process.env.TRACKER_ISSUE_ID,
+  process.env.TRACKER_ORG_ID,
+  contents
+);
