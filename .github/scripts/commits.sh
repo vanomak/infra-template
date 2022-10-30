b2fb393022f8c34f3fcf3ff7ed0b1c45bfe38ecf
Ivan Makarov <vanomak@gmail.com>
2022-10-31 00:33:44 +1000

    Исправлена ошибка со списком коммитов

--- .github/scripts/commits.sh
+++ .github/scripts/commits.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-tag1=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | head -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
-tag2=$(git for-each-ref refs/tags/ --count=2 --sort=-refname | tail -1 | grep -oP -m 1 'rc-[0-9]+.[0-9]+.[0-9]+')
-git log --pretty=format:"Ответственный за релиз: %an%n" $tag1...$tag2 | head -1
-echo "коммиты, попавшие в релиз:"
+tag1=$(git tag --sort=-version:refname | head -1)
+tag2=$(git tag --sort=-version:refname | head -2 | tail -1)
+git log --pretty=format:"Ответственный за релиз: %an%n" | head -1
+echo "коммиты, попавшие в релиз: "
 git log --pretty=format:"%H %an %s%n" $tag2...$tag1
